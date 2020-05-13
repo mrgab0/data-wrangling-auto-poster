5e7fcbd7b2e6022ff5c8b14ef36edfa18e84a8ce
mrgab0 <iirockalonso@gmail.com>
2020-05-12 21:54:00 -0400

    adding some createOn fields to the mongoose model

--- Database/modeloDatos.js
+++ Database/modeloDatos.js
@@ -5,16 +5,17 @@ const Schema = mongoose.Schema;
 
 
 var modeloDatosSchema = Schema({
-	
+					
+					createdOn: { type: Date, default: Date.now, unique:true },
 					"dia": String,
 					"mes": String,
-					"año": String,
-					"promedio": String,					
+					"año": Number,
+					"promedio": Number,					
 					"moneda": String, 									
 					"agencia": String,
-					"precio": String,
+					"precio": Number,
 					"igual": String,
-					"porcentaje": String	
+					"porcentaje": Number	
 });
 
 module.exports = mongoose.model('modeloDatos', modeloDatosSchema); 
